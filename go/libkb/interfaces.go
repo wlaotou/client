05acc59f76c39a39dd4249a4cec36852328ac615
Antonio Marcedone <a.marcedone@gmail.com>
2018-07-11 18:38:23 -0400

    Removed Self from keyfinder args (it is deduced from context). Added more tests.

--- go/libkb/interfaces.go
+++ go/libkb/interfaces.go
@@ -804,7 +804,6 @@ type SaltpackRecipientKeyfinderEngineInterface interface {
 
 type SaltpackRecipientKeyfinderArg struct {
 	Recipients    []string // Could be users (even as assertions) or teams
-	Self          *User
 	NoSelfEncrypt bool
 	UseEntityKeys bool // Both per user and per team keys (and implicit teams for non existing users)
 	UsePaperKeys  bool
